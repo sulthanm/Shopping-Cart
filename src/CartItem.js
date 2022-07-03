7c4e88b45a0488906585ca0ebd1d312db6ef7e39
Mogal Sulthan <mogal_sulthan@epam.com>
2022-07-03 20:50:26 +0530

    add increase quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,7 +10,11 @@ class CartItem extends React.Component {
             qty : 1
         }
     }
-
+    increaseItem = () => {
+        //Arrow function helps to bind the Cartitem state , 
+        //simple nothing but we can use state of CartItem component in increaseItem function, else we cannt.  
+        console.log('state', this.state);    
+    } 
     render () {
         const {title, price, qty} = this.state;
         return (
@@ -23,9 +27,22 @@ class CartItem extends React.Component {
                     <div style={ {color : '#ccc'} } >{ price }</div>
                     <div style={ {fontSize : '#777'} } >Qty : { qty }</div>
                     <div className='cart-item-actions'>
-                        <img alt='decrease' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/929/929430.png' />
-                        <img alt='increase' className='action-icons' src='https://cdn-icons.flaticon.com/png/512/4210/premium/4210903.png?token=exp=1656855830~hmac=e2edf8f1d2f75c5f40dbe36a81c2e95b' />
-                        <img alt='delete' className='action-icons' src='https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg' />
+                        <img 
+                            alt='decrease' 
+                            className='action-icons' 
+                            src='https://cdn-icons-png.flaticon.com/512/929/929430.png' 
+                            onClick={this.increaseItem}
+                        />
+                        <img 
+                            alt='increase' 
+                            className='action-icons' 
+                            src='https://cdn-icons.flaticon.com/png/512/4210/premium/4210903.png?token=exp=1656855830~hmac=e2edf8f1d2f75c5f40dbe36a81c2e95b' 
+                        />
+                        <img 
+                            alt='delete' 
+                            className='action-icons' 
+                            src='https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg' 
+                        />
                     </div>
                 </div>
             </div>
